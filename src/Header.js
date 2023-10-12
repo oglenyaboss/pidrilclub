2fa2d3cc878609f3ce31c380b4a879f32663ee5f
lenya lozhkin <oglenyaboss@icloud.com>
2023-10-12 17:46:24 +0700

    second
    added smthng

--- src/Header.js
+++ src/Header.js
@@ -10,13 +10,13 @@ export default function Header() {
                 className="header--image"
             />
             <h2 className="header--title">PidrilClub.ru</h2>
-            <a href={"https://github.com/oglenyaboss"} className="social--icon">
+            <a href={"https://github.com/oglenyaboss"} target="_blank" className="social--icon">
                 <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/github.png" alt="github"/>
             </a>
-            <a href={"https://vk.com/sashatop01"} className={"social--icon"}>
+            <a href={"https://vk.com/sashatop01"} target="_blank" className={"social--icon"}>
                 <img width="50" height="50" src="https://img.icons8.com/ios/50/vk-circled--v1.png" alt="vk-circled--v1"/>
             </a>
-            <a href={"https://t.me/pidrilny_club"} className={"social--icon"}>
+            <a href={"https://t.me/pidrilny_club"} target="_blank" className={"social--icon"}>
                 <img width="50" height="50" src="https://img.icons8.com/ios/50/telegram.png" alt="telegram"/>
             </a>
         </header>
